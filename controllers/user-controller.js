609e7042ce895bc42fb7f0fbebf8c290891ee412
Rick Martin <rmartin1985@att.net>
2021-12-29 17:21:45 -0600

    GET one user by id

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -26,5 +26,29 @@ const userController = {
             .catch(err => res.status(400).json(err));
     },
 
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id.' })
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
 
 }
