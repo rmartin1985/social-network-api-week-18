3c394e94a2775b0b5708fc5fa56a16fd29e5d235
Rick Martin <rmartin1985@att.net>
2021-12-29 17:17:41 -0600

    GET all users and POST a new user

---
+++ controllers/user-controller.js
@@ -0,0 +1,30 @@
+const { User } = require('../models');
+
+const userController = {
+    getAllUsers(req, res) {
+        User.find({})
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
+
+
+}
