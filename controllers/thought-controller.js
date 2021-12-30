70f9705c56dd74f6953dfd813b11f2f26e456cef
Rick Martin <rmartin1985@att.net>
2021-12-29 18:55:04 -0600

    delete a reaction

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -105,4 +105,20 @@ const thoughtController = {
                 res.status(400).json(err);
             })
     },
+
+    deleteReaction({ params }, res) {
+        Thought.findOneAndUpdate({ _id: params.thoughtId }, { $pull: {reactions: { reactionId: params.reactionId } } }, { new: true, })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id.'});
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            })
+    }
 }
+
+module.exports = thoughtController;
