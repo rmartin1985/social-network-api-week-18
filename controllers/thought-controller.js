bd0afb45ea49ef985b39272b69e0cbd581e53620
Rick Martin <rmartin1985@att.net>
2021-12-29 18:39:54 -0600

    update a thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -50,5 +50,25 @@ const thoughtController = {
                 console.log(err);
                 res.status(400).json(err)
             })
+    },
+
+    updateThought({params, body}, res) {
+        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .populate({
+                path: 'reactions',
+                seleect: '-__v'
+            })
+            .select('-__v')
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id.' })
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err)
+            })
     }
 }
