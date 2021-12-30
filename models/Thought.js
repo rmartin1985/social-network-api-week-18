23ce826abfc6597bb29443298d63dce9326ffc23
Rick Martin <rmartin1985@att.net>
2021-12-29 19:19:29 -0600

    moved ReactionSchema to top to fix error

--- models/Thought.js
+++ models/Thought.js
@@ -1,33 +1,6 @@
 const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
 
-const ThoughtSchema = new Schema({
-    thoughtText: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 280
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (createdAtVal) => moment(createdAtVal).format('MM DD, YYYY hh:mm a')
-    },
-    username: {
-        type: String,
-        required: true
-    },
-    reactions: [ReactionSchema]
-},
-    {
-        toJSON: {
-            virtual: true,
-            getters: true
-        },
-        id: false
-    }
-)
-
 const ReactionSchema = new Schema({
     reactionId: {
         type: Schema.Types.ObjectId,
@@ -54,6 +27,33 @@ const ReactionSchema = new Schema({
         }
     }
 );
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MM DD, YYYY hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+},
+    {
+        toJSON: {
+            virtual: true,
+            getters: true
+        },
+        id: false
+    }
+)
 
 ThoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
